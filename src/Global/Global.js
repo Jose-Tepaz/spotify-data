e376ef65aa3d26351e2747c5ea8aa4c3bdd4555a
Jose-Tepaz <josetepaz28@gmail.com>
2024-09-13 17:02:11 -0600

    subirndo poryecto a git hub

--- src/Global/Global.js
+++ src/Global/Global.js
@@ -1,12 +1,12 @@
 var Global = {
-	playlistLimit: 50,
-	songLimit: 100,
-	access_token: "",
-	refresh_token: "",
-	redirect_uri : "http://localhost:3000/",
-	client_id : "ae8a512d82a744399ab3c26d9f3f7032",
-	client_secret : "0d076cf3819a4884842d68cb0b923661",
-	scopes : "user-read-private user-read-email playlist-read-private"
+    playlistLimit: 50,
+    songLimit: 100,
+    access_token: "",
+    refresh_token: "",
+    redirect_uri: "http://localhost:3000/",
+    client_id: "8b338fa944994fe99911aa6779421146",
+    client_secret: "300acf4d49a34b91ac4f7daeb49438e0",
+    scopes: "user-read-private user-read-email playlist-read-private"
 }
 
 export default Global;
